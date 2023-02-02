a36eff3029c78827a51901d66f432d67dc1d5f8f
kimsh <ktftjdgh@naver.com>
2023-02-02 13:03:47 +0900

    최초생성

---
+++ src/test/java/com/multi/ukids/UkidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.ukids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
