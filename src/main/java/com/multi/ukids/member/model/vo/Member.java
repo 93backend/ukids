1f51811f05d7fc4e648a6117b86becc87cfd9f62
myror <myror@DESKTOP-8DDK7P2>
2023-02-03 14:34:25 +0900

    Member.java와 member-mapper.xml 커밋

--- src/main/java/com/multi/ukids/member/model/vo/Member.java
+++ src/main/java/com/multi/ukids/member/model/vo/Member.java
@@ -1,5 +1,24 @@
 package com.multi.ukids.member.model.vo;
 
-public class Member {
+import java.util.Date;
 
+public class Member {
+	
+	private int memberNo;
+	private String id;
+	private String password;
+	private String role;
+	private String name;
+	private String postCode;
+	private String address;
+	private String email;
+	private String phone;
+	private int kidNum;
+	private int kidAge1; 
+	private int kidAge2; 
+	private int kidAge3;
+	private String kakaoToken;
+	private String STATUS;
+	private Date enrollDate;
+	private Date modifyDate;
 }
