9feb64c14ceaf88f3cc7be0ee861899ec0b6a94f
kimsh <ktftjdgh@naver.com>
2023-02-03 13:09:12 +0900

    회원 패키지 구조 생성 / 김성호 / 02-03 13:07  [주요내용]  	- 회원 쪽 패키지 구조 생성  	- Member.java  	- MemberController.java  	- MemberMapper.java  	- MemberService.java

---
+++ src/main/java/com/multi/ukids/member/model/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.multi.ukids.member.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+}
---
+++ src/main/java/com/multi/ukids/member/model/vo/Member.java
@@ -0,0 +1,5 @@
+package com.multi.ukids.member.model.vo;
+
+public class Member {
+
+}
