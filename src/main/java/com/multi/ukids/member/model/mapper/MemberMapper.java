1611b1cd7b39e56db1862544811351378af9b20a
myror <myror@DESKTOP-8DDK7P2>
2023-02-03 15:15:22 +0900

    MemberMapper 코드/박태성/02-03 15:14 MemberMapper 인터페이스 작성

--- src/main/java/com/multi/ukids/member/model/mapper/MemberMapper.java
+++ src/main/java/com/multi/ukids/member/model/mapper/MemberMapper.java
@@ -1,8 +1,22 @@
 package com.multi.ukids.member.model.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.multi.ukids.member.model.vo.Member;
 
 @Mapper
-public class MemberMapper {
+public interface MemberMapper {
+	
+	int selectCount();
+	List<Member> selectAll();
+	Member selectMember(@Param("id") String id); // @Param : 파라메터임을 알리는 어노테이션. 없어 된다.
+	int insertMember(Member member);
+	int updateMember(Member member);
+	int updatePwd(Map<String, String> map);
+	int deleteMember(int no);
 
 }
