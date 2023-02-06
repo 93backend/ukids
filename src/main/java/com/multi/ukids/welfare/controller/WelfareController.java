df79ebe6abc9a2001ea2a61c24d6967809f11ceb
rncnr <rncnr@host.docker.internal>
2023-02-06 23:57:03 +0900

    아동복지시설 메인, 상세 / 유채원 / 02-06 23:56 [주요내용] - 아동복지시설 이미지 추가 	wf10.jpg ~ wf20.jpg
    - 메인, 상세 시설 이미지 크기가 달라져도 레이아웃 고정되도록 수정
    	welfare-main.css, welfare-detail.css
    
    - 메인, 상세 랜덤으로 이미지 뜨도록 수정
    - 메인, 상세 업종명 수정
    - 상세 이메일 수정
    	welfare-main.html, welfare-detail.html,
    	WelfareMapper.java, WelfareController.java

--- src/main/java/com/multi/ukids/welfare/controller/WelfareController.java
+++ src/main/java/com/multi/ukids/welfare/controller/WelfareController.java
@@ -1,5 +1,6 @@
 package com.multi.ukids.welfare.controller; 
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +25,23 @@ public class WelfareController {
 	@Autowired
 	private WelfareService welfareService;
 	
+	//이미지 인덱스 랜덤 생성
+	public int[] setRandomImgIdx(int start, int term) {
+		int[] idx = new int[9];
+		idx[0] = start;
+		for(int i=0; i<idx.length; i++) {
+			if (i == 0) continue;
+			idx[i] = idx[i-1] + term;
+			
+			if (idx[i] % 21 != 0) {
+				idx[i] %= 21;
+			} else {
+				idx[i] = start + 1; 
+			}
+		}
+		return idx;
+	}
+	
 	@GetMapping("/welfare-main")
 	public String welfareMain(Model model, @RequestParam Map<String, String> param) {
 		int page = 1;
@@ -32,38 +50,57 @@ public class WelfareController {
 			page = Integer.parseInt(param.get("page"));
 		} catch (Exception e) {}
 		
+		//아동복지시설 목록
 		int welfareCount = welfareService.getWelfareCount(param);
 		PageInfo pageInfo = new PageInfo(page, 5, welfareCount, 9);
 		List<Welfare> list = welfareService.getWelfareList(pageInfo, param);
+		
+		//아동복지시설 랜덤 이미지
+		int[] imgIdx = setRandomImgIdx(page, 2);
 		
 		model.addAttribute("count", welfareCount);
 		model.addAttribute("list", list);
 		model.addAttribute("param", param);
 		model.addAttribute("pageInfo", pageInfo);
+		model.addAttribute("imgIdx", imgIdx);
 		
 //		log.info("param : " + param.toString());
 //		log.info("welfareCount : " + Integer.toString(welfareCount));
+//		log.info(Arrays.toString(imgIdx));
 		
 		return "welfare-main";
 	}
 	
 	@GetMapping("/welfare-detail")
-	public String welfareDetail(Model model, @RequestParam("no") int no) {
+	public String welfareDetail(Model model, @RequestParam("no") int no, @RequestParam("img") int imgIdx) {
 		Welfare welfare = welfareService.findByNo(no);
 		if(welfare == null) {
 			return "redirect:error";
 		}
 		
-		//상세정보
+		//상세정보 설립일자, 이메일 형식 변경
 		String estbDe = welfare.getEstbDe();
 		String formattedEstbDe = estbDe.substring(0, 4) + "."  + estbDe.substring(4, 6) + "." + estbDe.substring(6, 8);
+		String mailAddr = welfare.getFcltMailAddr();
+		String formattedMailAddr = "";
+		if (mailAddr.length() > 4) {
+			formattedMailAddr = mailAddr.substring(0, 8) + "@facility.com";
+		} else {
+			formattedMailAddr = mailAddr.substring(0, 4) + "@facility.com";
+		}
 		
-		//근처아동복지시설
+		//근처아동복지시설 목록
 		List<Welfare> nearWelfareList = welfareService.getNearWelfareList(welfare);
 		Collections.shuffle(nearWelfareList);
 		
+		//근처아동복지시설 랜덤 이미지
+		int[] nearWfImgIdx = setRandomImgIdx(imgIdx, 3);
+		
 		model.addAttribute("welfare", welfare);
+		model.addAttribute("imgIdx", imgIdx);
+		model.addAttribute("nearWfImgIdx", nearWfImgIdx);
 		model.addAttribute("formattedEstbDe", formattedEstbDe);
+		model.addAttribute("formattedMailAddr", formattedMailAddr);
 		model.addAttribute("nearWelfareList", nearWelfareList);
 		
 //		for (Welfare data : nearWelfareList) {
@@ -73,6 +110,7 @@ public class WelfareController {
 		
 		return "welfare-detail";
 	}
+
 	
 //	@GetMapping("/error")
 //	public String error() {
