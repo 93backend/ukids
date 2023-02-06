e7a19117f8d856ee0abb8107adc6f13ee64d35a6
User <User@118.220.113.4>
2023-02-06 22:01:17 +0900

    admission vo 추가 / 조규완 / 02-06 22:00 KAdmission.java, NAdmission.java mypage 사이드바 연결 수정

---
+++ src/main/java/com/multi/ukids/kinder/model/vo/KAdmission.java
@@ -0,0 +1,19 @@
+package com.multi.ukids.kinder.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KAdmission {
+	private int no;
+	private int kinNo;
+	private int memberNo;
+	private String status;
+	private Date hopeDate;
+	private Date enrollDate;
+}
---
+++ src/main/java/com/multi/ukids/nursery/model/vo/NAdmission.java
@@ -0,0 +1,19 @@
+package com.multi.ukids.nursery.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NAdmission {
+	private int no;
+	private int nuNo;
+	private int memberNo;
+	private String status;
+	private Date hopeDate;
+	private Date enrollDate;
+}
